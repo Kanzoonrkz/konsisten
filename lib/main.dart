23b9ed001d6d03436a70ed418f5d3e77e883b2f1
Kanzoonrkz <kanzoonrkz@gmail.com>
2021-08-02 02:42:25 +0700

    initialized 4 main page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:konsisten/pages/activitiespage.dart';
+import 'package:konsisten/pages/historypage.dart';
+import 'package:konsisten/pages/homepage.dart';
+import 'package:konsisten/pages/profilepage.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,9 +13,34 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'konsisten',
       theme: ThemeData(),
-      home: Scaffold(),
+      home: MainPage(),
     );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int index = 0;
+
+  //Page lists
+  final pages = <Widget>[
+    HomePage(),
+    HistoryPage(),
+    ActivitiesPage(),
+    ProfilePage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
   }
 }
